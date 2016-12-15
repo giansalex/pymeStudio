3ba94e9c35dfb64cc246254f0a306e701cfa7b89
giansalex <giansalex@gmail.com>
2016-12-15 15:48:07 -0500

    User Login & Register

---
+++ src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrador
+ * Date: 15/12/2016
+ * Time: 12:16 PM
+ */
+
+namespace AppBundle\Repository;
+use Doctrine\ORM\EntityRepository;
+/**
+ * Class UserRepository
+ * @package AppBundle\Repository*
+ */
+class UserRepository extends EntityRepository
+{
+}
